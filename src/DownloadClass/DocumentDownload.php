d9e53200d11f3e07b3e1bcb94e7946c8b685df8e
EugenK92 <e.k.92@gmx.de>
2017-07-17 17:15:36 +0200

    DocumentDownload

---
+++ src/DownloadClass/DocumentDownload.php
@@ -0,0 +1,95 @@
+<?php
+
+class DocumentDownload {
+    private $rootdir;
+    private $uploaddir;
+    private $chunkWhole;
+    private $chunkpart;
+
+    public function __construct() {
+        $config = simplexml_load_file(__DIR__ . "/../../config/config.xml") or die("Error: Cannot create object");
+        $this->rootdir = (string)$config->RootDirectory;
+        $this->uploaddir = (string)$config->UploadDirectory;
+        $this->chunkWhole = (int)$config->WholeChunkSize;
+        $this->chunkpart = (int)$config->PartChunkSize;
+    }
+
+    private function getContentType($file) {
+        $finfo = finfo_open(FILEINFO_MIME_TYPE);
+        return finfo_file($finfo, $file);
+    }
+
+    public function downloadWholeDocument($name) {
+        $file = $this->uploaddir . '/' . $name;
+        $contentType = $this->getContentType($file);
+        $out = fopen($file, "r");
+        $len = $this->chunkWhole;
+        header('Content-Description: File Transfer');
+        header('Content-Type: ' . $contentType);
+        header('Content-Length: ' . filesize($file));
+        while (!feof($out)) {
+            $buffer = fread($out, $len);
+            ob_flush();
+            flush();
+            echo $buffer;
+        }
+        fclose($out);
+        exit;
+    }
+
+    public function downloadPartDocument($name, $part) {
+        $file = $this->uploaddir . '/' . $name;
+        $contentType = $this->getContentType($file);
+        $out = fopen($file, "r");
+
+        //===============================================================================//
+        $size = $this->chunkpart;
+        // if filesize is greater
+        // than the splitsize = size
+        // else the splitsize = filesize
+        $splitSize = filesize($file) >= $size ? $size : filesize($file);
+
+        // if key exists
+        // part = key
+        // else part = 0
+        $part = $part != null ? $part : 0;
+        // if part == 0
+        // part = 0
+        // else part = part - 1
+        $part = $part <= 0 ? 0 : $part - 1;
+
+        $filesize = filesize($file);
+        $fileparts = ceil( $filesize / $splitSize);
+
+        if ($part >= $fileparts) {
+            throw new Exception("Parts out of bounds!", 404);
+        }
+
+        $currentPosition = $splitSize * $part;
+
+        //Calculate the rest of the file, if requested part ist larger then filesize
+        $end = $splitSize * ($part + 1);
+        if ($end > $filesize) {
+            $splitSize = ($filesize - $currentPosition);
+        }
+        header('Content-Description: File Transfer');
+        header('Content-Type: ' . $contentType);
+        header('Content-Length: ' . $splitSize);
+        header('HTTP_Content-Size: ' . $fileparts);
+        header('HTTP_Current-Part: ' . ($part + 1));
+
+
+        //Set Pointer in File (Read from that pointer)
+        fseek($out, $currentPosition, SEEK_SET);
+
+        if (!feof($out)) {
+            $buffer = fread($out, $splitSize);
+            ob_flush();
+            flush();
+            echo $buffer;
+        }
+        fclose($out);
+        exit;
+    }
+
+}
